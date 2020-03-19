2375289dee008425d26c72b0707bc23fb6437e62
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-03-19 12:46:54 +0530

    initial commit

--- setup.bat
+++ setup.bat
@@ -1,13 +1,20 @@
 @ECHO OFF 
 :: This batch file install basic requirements listed in requirements.txt
 TITLE Setting up things
-:: Section 1: Some information.
 python --version
 pip --version
+ECHO Upgrading pip
 python -m pip install --upgrade pip
+ECHO installing Packages
 pip install numpy
 pip install matplotlib
 pip install pandas
 pip install scikit-learn
+ECHO Upgrading Packages
+pip install --upgrade pandas
+pip install --upgrade numpy
+pip install --upgrade matplotlib
+pip install --upgrade scikit-learn
+ECHO All Done
 python "Simple Linear Regression.py"
 PAUSE
