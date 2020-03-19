ae102f9dddd8cb6cff0124ef416fe4d1f4994d28
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-03-19 16:13:29 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -12,30 +12,18 @@ pip install pandas
 pip install scikit-learn
 python "Simple Linear Regression.py"
 ECHO Uninstalling Packages
-pip uninstall pandas
-y
-pip uninstall cycler
-y
-pip uninstall joblib
-y
-pip uninstall kiwisolver
-y
-pip uninstall pyparsing
-y
-pip uninstall python-dateutil
-y
-pip uninstall pytz
-y
-pip uninstall scipy
-y
-pip uninstall six
-y
-pip uninstall numpy
-y
-pip uninstall matplotlib
-y
-pip uninstall scikit-learn
-y
+pip uninstall pandas /y
+pip uninstall cycler /y
+pip uninstall joblib /y
+pip uninstall kiwisolver /y
+pip uninstall pyparsing /y
+pip uninstall python-dateutil /y
+pip uninstall pytz /y
+pip uninstall scipy /y
+pip uninstall six /y
+pip uninstall numpy /y
+pip uninstall matplotlib /y
+pip uninstall scikit-learn /y
 ECHO All Done
 cd..
 final.bat
